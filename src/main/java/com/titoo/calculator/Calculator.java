3dd646956bb69bd2921179989077155c527fe376
s-titoo <sanyatito@gmail.com>
2025-09-13 07:30:56 +0200

    Add comment

--- src/main/java/com/titoo/calculator/Calculator.java
+++ src/main/java/com/titoo/calculator/Calculator.java
@@ -1,6 +1,12 @@
 package com.titoo.calculator; 
 import org.springframework.stereotype.Service; 
  
+/**
+ * Service class providing basic calculator operations.
+ * <p>
+ * Currently supports summing two integers.
+ * </p>
+ */
 @Service 
 public class Calculator { 
      public int sum(int a, int b) { 
